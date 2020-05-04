d96e937fca32e4268d3c6a6ab88a3775d488e658
jonathanking <jonathanking@berkeley.edu>
2020-05-04 12:03:38 -0400

    Turn off ProDy debug verbosity.

--- sidechainnet/create.py
+++ sidechainnet/create.py
@@ -8,7 +8,7 @@ import argparse
 import re
 
 import prody as pr
-pr.confProDy(verbosity="error")
+pr.confProDy(verbosity="warning")
 
 from sidechainnet.download_and_parse import download_sidechain_data
 from sidechainnet.utils.proteinnet import parse_raw_proteinnet
