7a27988d105de2c635318cae1857dbddbdc7c0f7
jonathanking <jonathanking@berkeley.edu>
2020-10-08 15:29:08 -0700

    Add structure/PDB creation files to repo from protein-transformer.

---
+++ sidechainnet/structure/PDB_Creator.py
@@ -0,0 +1,334 @@
+import numpy as np
+import pymol
+import torch
+import wandb
+from prody import calcTransformation
+
+from sidechainnet.utils.sequence import ONE_TO_THREE_LETTER_MAP
+import sidechainnet
+from sidechainnet.structure.Structure import angles_to_coords
+from sidechainnet.structure.build_info import SC_BUILD_INFO, BB_BUILD_INFO, NUM_COORDS_PER_RES
+from sidechainnet.structure.Structure import nerf
+from sidechainnet.structure.StructureBuilder import StructureBuilder
+
+
+class PDB_Creator(object):
+    """
+    A class for creating PDB files given an atom mapping and coordinate set.
+    The general idea is that if any model is capable of predicting a set of
+    coordinates and mapping between those coordinates and residue/atom names,
+    then this object can be use to transform that output into a PDB file.
+
+    The Python format string was taken from http://cupnet.net/pdb-format/.
+    """
+
+    def __init__(self,
+                 coords,
+                 seq=None,
+                 mapping=None,
+                 atoms_per_res=NUM_COORDS_PER_RES):
+        """
+        Input:
+            coords: (L x N) x 3 where L is the protein sequence len and N
+                    is the number of atoms/residue in the coordinate set
+                    (atoms_per_res).
+            mapping: length L list of list of atom names per residue,
+                     i.e. (RES -> [ATOM_NAMES_FOR_RES])
+        Output:
+            saves PDB file to disk
+        """
+
+        self.coords = coords
+        if seq and not mapping:
+            if len(seq) == coords.shape[0] / atoms_per_res:
+                raise AssertionError(
+                    "The sequence length must match the coordinate length and contain 1 "
+                    "letter AA codes." + str(coords.shape[0] / atoms_per_res) +
+                    " " + str(len(seq)))
+            else:
+                self.seq = seq
+                self.mapping = self._make_mapping_from_seq()
+        elif not seq and not mapping:
+            raise Exception("Please provide a seq or a mapping.")
+        elif mapping and not seq:
+            self.mapping = mapping
+            self.seq = self._get_seq_from_mapping()
+        assert type(self.mapping[0][0]) == str and len(
+            self.mapping[0]
+            [0]) == 1, "1 letter AA codes must be used in the mapping."
+        self.atoms_per_res = atoms_per_res
+        self.format_str = (
+            "{:6s}{:5d} {:^4s}{:1s}{:3s} {:1s}{:4d}{:1s}   "
+            "{:8.3f}{:8.3f}{:8.3f}{:6.2f}{:6.2f}          {:>2s}{:2s}")
+        self.atom_nbr = 1
+        self.res_nbr = 1
+        self.defaults = {
+            "alt_loc": "",
+            "chain_id": "",
+            "insertion_code": "",
+            "occupancy": 1,
+            "temp_factor": 0,
+            "element_sym": "",
+            "charge": ""
+        }
+
+        if self.coords.shape[0] % self.atoms_per_res != 0:
+            raise AssertionError(f"Coords is not divisible by {atoms_per_res}. "
+                                 f"{self.coords.shape}")
+
+    def _coord_generator(self):
+        """
+        A generator that yields ATOMS_PER_RES atoms at a time from self.coords.
+        """
+        coord_idx = 0
+        while coord_idx < self.coords.shape[0]:
+            yield self.coords[coord_idx:coord_idx + self.atoms_per_res]
+            coord_idx += self.atoms_per_res
+
+    def _get_line_for_atom(self,
+                           res_name,
+                           atom_name,
+                           atom_coords,
+                           missing=False):
+        """
+        Returns the 'ATOM...' line in PDB format for the specified atom. If
+        missing, this function should have special, but not yet determined,
+        behavior.
+        """
+        if missing:
+            occupancy = 0
+        else:
+            occupancy = self.defaults["occupancy"]
+        return self.format_str.format(
+            "ATOM", self.atom_nbr, atom_name, self.defaults["alt_loc"],
+            ONE_TO_THREE_LETTER_MAP[res_name], self.defaults["chain_id"],
+            self.res_nbr, self.defaults["insertion_code"], atom_coords[0],
+            atom_coords[1], atom_coords[2], occupancy,
+            self.defaults["temp_factor"], atom_name[0], self.defaults["charge"])
+
+    def _get_lines_for_residue(self, res_name, atom_names, coords):
+        """
+        Returns PDB-formated lines for all atoms in this residue. Calls
+        get_line_for_atom.
+        """
+        residue_lines = []
+        for atom_name, atom_coord in zip(atom_names, coords):
+            # TODO: what to do in the PDB file if atom is missing?
+            if atom_name is "PAD" or np.isnan(
+                    atom_coord).sum() > 0 or atom_coord.sum() == 0:
+                continue
+            # if np.isnan(atom_coord).sum() > 0:
+            #     residue_lines.append(self.get_line_for_atom(res_name, atom_name, atom_coord,
+            #     missing=True))
+            #     self.atom_nbr += 1
+            #     continue
+            residue_lines.append(
+                self._get_line_for_atom(res_name, atom_name, atom_coord))
+            self.atom_nbr += 1
+        return residue_lines
+
+    def _get_lines_for_protein(self):
+        """
+        Returns PDB-formated lines for all residues in this protein. Calls
+        get_lines_for_residue.
+        """
+        self.lines = []
+        self.res_nbr = 1
+        self.atom_nbr = 1
+        mapping_coords = zip(self.mapping, self._coord_generator())
+        for (res_name, atom_names), res_coords in mapping_coords:
+            self.lines.extend(
+                self._get_lines_for_residue(res_name, atom_names, res_coords))
+            self.res_nbr += 1
+        return self.lines
+
+    @staticmethod
+    def _make_header(title):
+        """
+        Returns the PDB header.
+        """
+        return f"REMARK  {title}"
+
+    @staticmethod
+    def _make_footer():
+        """
+        Returns the PDB footer.
+        """
+        return "TER\nEND          \n"
+
+    def _make_mapping_from_seq(self):
+        """
+        Given a protein sequence, this returns a mapping that assumes coords
+        are generated in groups of 13, i.e. the output is L x 13 x 3.
+        """
+        mapping = []
+        for residue in self.seq:
+            mapping.append((residue, ATOM_MAP_14[residue]))
+        return mapping
+
+    def save_pdb(self, path, title="test"):
+        """
+        Given a file path and title, this function generates the PDB lines,
+        then writes them to a file.
+        """
+        self._get_lines_for_protein()
+        self.lines = [self._make_header(title)
+                     ] + self.lines + [self._make_footer()]
+        with open(path, "w") as outfile:
+            outfile.write("\n".join(self.lines))
+
+    def save_gltf(self, path, title="test", create_pdb=False):
+        """
+        This function first creates a PDB file, then converts it to a GLTF
+        (3D Object) file. Used for visualizign with Weights and Biases. """
+        assert ".gltf" in path, "requested filepath must end with '.gtlf'."
+        if create_pdb:
+            self.save_pdb(path.replace(".gltf", ".pdb"), title)
+        pymol.cmd.load(path.replace(".gltf", ".pdb"), title)
+        pymol.cmd.color("oxygen", title)
+        pymol.cmd.save(path, quiet=True)
+        pymol.cmd.delete("all")
+
+    def save_gltfs(self,
+                   path1,
+                   path2,
+                   gltf_out_path,
+                   make_pse=False,
+                   pse_out_path="",
+                   make_png=False):
+        """
+        This function first creates a PDB file, then converts it to a GLTF
+        (3D Object) file. Used for visualizign with Weights and Biases. """
+        assert ".pdb" in path1, "requested filepaths must end with '.pdb'."
+        pymol.cmd.load(path1, "true")
+        pymol.cmd.load(path2, "pred")
+        pymol.cmd.color("marine", "true")
+        pymol.cmd.color("oxygen", "pred")
+        rmsd, _, _, _, _, _, _ = pymol.cmd.align("true", "pred", quiet=True)
+        try:
+            pymol.cmd.save(gltf_out_path, f"{wandb.run.step:05}.gltf")
+        except TypeError:
+            # Addresses an issue where the PyMol scene may not be setup correctly
+            pymol.cmd.delete("all")
+            return
+
+        # Align and save PSE
+        if make_pse:
+            pymol.cmd.show("lines")
+            pymol.cmd.save(pse_out_path, quiet=True)
+            pymol.cmd.save(pse_out_path.replace(".pse", ".pdb"), quiet=True)
+        if make_png:
+            pymol.cmd.png(gltf_out_path.replace("gltf", "png"),
+                          width=400,
+                          ray=0)
+
+        pymol.cmd.delete("all")
+
+    def _get_seq_from_mapping(self):
+        """
+        Returns the protein sequence in 1-letter AA codes.
+        """
+        return "".join([m[0] for m in self.mapping])
+
+
+if NUM_COORDS_PER_RES == 13:
+    ATOM_MAP_13 = {}
+    for one_letter in ONE_TO_THREE_LETTER_MAP.keys():
+        ATOM_MAP_13[one_letter] = ["N", "CA", "C"] + list(
+            SC_BUILD_INFO[ONE_TO_THREE_LETTER_MAP[one_letter]]["atom-names"])
+        ATOM_MAP_13[one_letter].extend(["PAD"] *
+                                       (13 - len(ATOM_MAP_13[one_letter])))
+
+if NUM_COORDS_PER_RES == 14:
+    ATOM_MAP_14 = {}
+    for one_letter in ONE_TO_THREE_LETTER_MAP.keys():
+        ATOM_MAP_14[one_letter] = ["N", "CA", "C", "O"] + list(
+            SC_BUILD_INFO[ONE_TO_THREE_LETTER_MAP[one_letter]]["atom-names"])
+        ATOM_MAP_14[one_letter].extend(["PAD"] *
+                                       (14 - len(ATOM_MAP_14[one_letter])))
+
+
+def generate_pdbs_from_debug_dataset():
+    import torch
+
+    data = torch.load("debug_struct.pt")
+
+    seq, seq_gap = data["seq"]
+    ang, ang_gap = data["ang"]
+    crd, crd_gap = data["crd"]
+
+    creator_from_crd = PDB_Creator(crd, seq=seq)
+    creator_from_crd.save_pdb("from_crd.pdb")
+
+    creator_from_crd_gap = PDB_Creator(crd_gap, seq=seq_gap)
+    creator_from_crd_gap.save_pdb("from_crd_gap.pdb")
+
+    crd_from_ang = get_coordinates_from_numpy_data(seq, ang)
+    creator_from_ang = PDB_Creator(crd_from_ang, seq=seq)
+    creator_from_ang.save_pdb("from_ang.pdb")
+
+    crd_from_ang_gap = get_coordinates_from_numpy_data(seq_gap, ang_gap)
+    creator_from_ang_gap = PDB_Creator(crd_from_ang_gap, seq=seq_gap)
+    creator_from_ang_gap.save_pdb("from_ang_gap.pdb")
+
+
+def get_coordinates_from_numpy_data(seq, ang_sincos):
+    # Add batch dimension, make copy
+    ang_sincos_new = ang_sincos[np.newaxis, :]
+
+    # Compute angles in radians from sin/cos representaion
+    # ang_rad = inverse_trig_transform(
+    #     torch.tensor(ang_sincos_new, dtype=torch.float))[0]
+    ang_rad = ang_sincos
+    # Remove nans
+    ang_rad[torch.isnan(ang_rad)] = 0
+
+    if torch.isnan(ang_rad).any():
+        print("Nan in ang_rad.")
+
+    seq_as_ints = sidechainnet.sequence.VOCAB.str2ints(seq, add_sos_eos=False)
+    seq_as_ints = torch.tensor(seq_as_ints, dtype=torch.long)
+
+    coords = angles_to_coords(ang_rad, seq_as_ints, remove_batch_padding=False)
+    return coords.numpy()
+
+
+def make_debug_structure_dataset():
+    """ Creates a pytorch dictionary that contains one example of a structure
+        without a gap, and another with a gap. Prints their ProteinNet IDs.
+    """
+    import torch
+
+    data = torch.load("../../data/proteinnet/casp12_191101_100.pt")
+
+    nogap = 3
+    gap = 10  #5
+    seq, ang, crd = data["train"]["seq"][nogap], data["train"]["ang"][nogap], \
+                    data["train"]["crd"][nogap]
+    seq_gap, ang_gap, crd_gap = data["train"]["seq"][gap], data["train"]["ang"][
+        gap], \
+                                data["train"]["crd"][gap]
+    print(data["train"]["ids"][nogap])
+    print(data["train"]["ids"][gap])
+
+    d = {"seq": (seq, seq_gap), "ang": (ang, ang_gap), "crd": (crd, crd_gap)}
+    torch.save(d, "debug_struct.pt")
+
+
+if __name__ == "__main__":
+    # TODO don't reimplement dataloader?
+    # TODO do several predictions, add PDB name
+    # TODO add ability to predict given model checkpoint
+    # TODO CUDA isn't playing nice
+
+    # make_debug_structure_dataset()
+    # generate_pdbs_from_debug_dataset()
+
+    d = torch.load(
+        "/home/jok120/protein-transformer/data/proteinnet/casp12_200216_30.pt")
+    seq = d["train"]["seq"][10]
+    ang = d["train"]["ang"][10]
+    # ang = inverse_trig_transform(torch.tensor(ang, dtype=torch.float32))[:,
+    #                                                                      0, :]
+    sb = StructureBuilder(seq, ang)
+    sb.to_pdb("200210b.pdb")
---
+++ sidechainnet/structure/StructureBuilder.py
@@ -0,0 +1,286 @@
+import numpy as np
+import torch
+
+from sidechainnet.utils.sequence import VOCAB
+from sidechainnet.structure.build_info import SC_BUILD_INFO, BB_BUILD_INFO, NUM_COORDS_PER_RES, SC_ANGLES_START_POS
+from sidechainnet.structure.Structure import nerf
+
+
+class StructureBuilder(object):
+    """
+    Given angles and protein sequence, reconstructs a single protein's structure.
+
+    The hydroxyl-oxygen of terminal residues is not placed because this would
+    mean that the number of coordinates per residue would not be constant, or
+    cause other complications (i.e. what if the last atom of a structure is not
+    really a terminal atom because it's tail is masked out?). It is simpler to
+    ignore this atom for now.
+    """
+
+    def __init__(self, seq, ang, device=torch.device("cpu")):
+        """
+        Initialize a StructureBuilder for a single protein.
+
+        Parameters
+        ----------
+        seq : Tensor
+            An integer tensor (L) (without padding) that represents the protein's amino acid sequence.
+        ang : Tensor
+            An angle tensor (L X NUM_PREDICTED_ANGLES) that contain's all of the protein's interior angles.
+        device : device
+            The device on which to build the structure.
+        """
+        if type(seq) == str:
+            seq = torch.tensor([VOCAB._char2int[s] for s in seq])
+        self.seq = seq
+        self.ang = ang
+        self.device = device
+        self.coords = []
+        self.prev_ang = None
+        self.prev_bb = None
+        self.next_bb = None
+
+        self.pdb_creator = None
+        self.seq_as_str = None
+
+    def __len__(self):
+        return len(self.seq)
+
+    def iter_resname_angs(self, start=0):
+        for resname, angles in zip(self.seq[start:], self.ang[start:]):
+            yield resname, angles
+
+    def build_first_two_residues(self):
+        """ Constructs the first two residues of the protein. """
+        resname_ang_iter = self.iter_resname_angs()
+        first_resname, first_ang = next(resname_ang_iter)
+        second_resname, second_ang = next(resname_ang_iter)
+        first_res = ResidueBuilder(first_resname,
+                                   first_ang,
+                                   prev_res=None,
+                                   next_res=None)
+        second_res = ResidueBuilder(second_resname,
+                                    second_ang,
+                                    prev_res=first_res,
+                                    next_res=None)
+
+        # After building both backbones, use the second residue's N to build the first's CB
+        first_res.build_bb()
+        second_res.build()
+        first_res.next_res = second_res
+        first_res.build_sc()
+
+        return first_res, second_res
+
+    def build(self):
+        """
+        Construct all of the atoms for a residue. Special care must be taken
+        for the first residue in the sequence in order to place its CB, if
+        present.
+        """
+        # Build the first and second residues, a special case
+        first, second = self.build_first_two_residues()
+
+        # Combine the coordinates and build the rest of the protein
+        self.coords = first.stack_coords() + second.stack_coords()
+
+        # Build the rest of the structure
+        prev_res = second
+        for i, (resname, ang) in enumerate(self.iter_resname_angs(start=2)):
+            res = ResidueBuilder(resname, ang, prev_res=prev_res, next_res=None)
+            self.coords += res.build()
+            prev_res = res
+
+        self.coords = torch.stack(self.coords)
+
+        return self.coords
+
+    def get_seq_as_str(self):
+        if not self.seq_as_str:
+            self.seq_as_str = VOCAB.ints2str(map(int, self.seq))
+        return self.seq_as_str
+
+    def to_pdb(self, path, title="pred"):
+        if len(self.coords) == 0:
+            self.build()
+
+        if not self.pdb_creator:
+            from sidechainnet.structure.PDB_Creator import PDB_Creator
+            self.pdb_creator = PDB_Creator(self.coords.numpy(),
+                                           self.get_seq_as_str())
+
+        self.pdb_creator.save_pdb(path, title)
+
+
+class ResidueBuilder(object):
+
+    def __init__(self,
+                 name,
+                 angles,
+                 prev_res,
+                 next_res,
+                 device=torch.device("cpu")):
+        """Initialize a residue builder. If prev_{bb, ang} are None, then this
+        is the first residue.
+
+        Parameters
+        ----------
+        name : Tensor
+            The integer amino acid code for this residue.
+        angles : Tensor
+            Angle tensor containing necessary angles to define this residue.
+        prev_bb : Tensor, None
+            Coordinate tensor (3 x 3) of previous residue, upon which this residue is extending.
+        prev_ang : Tensor, None
+            Angle tensor (1 X NUM_PREDICTED_ANGLES) of previous reside, upon which this residue is extending.
+        """
+        assert type(name) == torch.Tensor, "Expected integer AA code." + str(
+            name.shape) + str(type(name))
+        if type(angles) == np.ndarray:
+            angles = torch.tensor(angles, dtype=torch.float32)
+        self.name = name
+        self.ang = angles.squeeze()
+        self.prev_res = prev_res
+        self.next_res = next_res
+        self.device = device
+
+        self.bb = []
+        self.sc = []
+        self.coords = []
+        self.coordinate_padding = torch.zeros(3)
+
+    def build(self):
+        self.build_bb()
+        self.build_sc()
+        return self.stack_coords()
+
+    def build_bb(self):
+        """ Builds backbone for residue. """
+        if self.prev_res is None:
+            self.bb = self.init_bb()
+        else:
+            pts = [
+                self.prev_res.bb[0], self.prev_res.bb[1], self.prev_res.bb[2]
+            ]
+            for j in range(4):
+                if j == 0:
+                    # Placing N
+                    t = self.prev_res.ang[4]  # thetas["ca-c-n"]
+                    b = BB_BUILD_INFO["BONDLENS"]["c-n"]
+                    dihedral = self.prev_res.ang[1]  # psi of previous residue
+                elif j == 1:
+                    # Placing Ca
+                    t = self.prev_res.ang[5]  # thetas["c-n-ca"]
+                    b = BB_BUILD_INFO["BONDLENS"]["n-ca"]
+                    dihedral = self.prev_res.ang[2]  # omega of previous residue
+                elif j == 2:
+                    # Placing C
+                    t = self.ang[3]  # thetas["n-ca-c"]
+                    b = BB_BUILD_INFO["BONDLENS"]["ca-c"]
+                    dihedral = self.ang[0]  # phi of current residue
+                else:
+                    # Placing O
+                    t = torch.tensor(BB_BUILD_INFO["BONDANGS"]["ca-c-o"])
+                    b = BB_BUILD_INFO["BONDLENS"]["c-o"]
+                    dihedral = self.ang[
+                        1] - np.pi  # opposite to psi of current residue
+
+                next_pt = nerf(pts[-3], pts[-2], pts[-1], b, t, dihedral)
+                pts.append(next_pt)
+            self.bb = pts[3:]
+
+        return self.bb
+
+    def init_bb(self):
+        """ Initialize the first 3 points of the protein's backbone. Placed in an arbitrary plane (z = .001). """
+        n = torch.tensor([0, 0, 0.001], device=self.device)
+        ca = n + torch.tensor([BB_BUILD_INFO["BONDLENS"]["n-ca"], 0, 0],
+                              device=self.device)
+        cx = torch.cos(np.pi - self.ang[3]) * BB_BUILD_INFO["BONDLENS"]["ca-c"]
+        cy = torch.sin(np.pi - self.ang[3]) * BB_BUILD_INFO["BONDLENS"]['ca-c']
+        c = ca + torch.tensor(
+            [cx, cy, 0], device=self.device, dtype=torch.float32)
+        o = nerf(n, ca, c, torch.tensor(BB_BUILD_INFO["BONDLENS"]["c-o"]),
+                 torch.tensor(BB_BUILD_INFO["BONDANGS"]["ca-c-o"]),
+                 self.ang[1] - np.pi)  # opposite to current residue's psi
+        return [n, ca, c, o]
+
+    def build_sc(self):
+        """
+        Builds the sidechain atoms for this residue.
+
+        Care is taken when placing the first sc atom (the beta-Carbon). This is
+        because the dihedral angle that places this atom must be defined using
+        a neighboring (previous or next) residue.
+        """
+        assert len(self.bb) > 0, "Backbone must be built first."
+        self.pts = {"N": self.bb[0], "CA": self.bb[1], "C": self.bb[2]}
+        if self.next_res:
+            self.pts["N+"] = self.next_res.bb[0]
+        else:
+            self.pts["C-"] = self.prev_res.bb[2]
+
+        last_torsion = None
+        for i, (bond_len, angle, torsion, atom_names) in enumerate(
+                get_residue_build_iter(self.name, SC_BUILD_INFO)):
+            # Select appropriate 3 points to build from
+            if self.next_res and i == 0:
+                a, b, c = self.pts["N+"], self.pts["C"], self.pts["CA"]
+            elif i == 0:
+                a, b, c = self.pts["C-"], self.pts["N"], self.pts["CA"]
+            else:
+                a, b, c = (self.pts[an] for an in atom_names[:-1])
+
+            # Select appropriate torsion angle, or infer it if it's part of a planar configuration
+            if type(torsion) is str and torsion == "p":
+                torsion = self.ang[SC_ANGLES_START_POS + i]
+            elif type(torsion) is str and torsion == "i" and last_torsion:
+                torsion = last_torsion - np.pi
+
+            new_pt = nerf(a, b, c, bond_len, angle, torsion)
+            self.pts[atom_names[-1]] = new_pt
+            self.sc.append(new_pt)
+            last_torsion = torsion
+
+        return self.sc
+
+    def stack_coords(self):
+        self.coords = self.bb + self.sc + (NUM_COORDS_PER_RES - \
+                                           len(self.bb) - len(self.sc)) * [self.coordinate_padding]
+        return self.coords
+
+    def __repr__(self):
+        return f"ResidueBuilder({VOCAB.int2char(int(self.name))})"
+
+
+def get_residue_build_iter(res, build_dictionary):
+    """
+    For a given residue integer code and a residue building data dictionary,
+    this function returns an iterator that returns 4-tuples. Each tuple
+    contains the necessary information to generate the next atom in that
+    residue's sidechain. This includes the bond lengths, bond angles, and
+    torsional angles.
+    """
+    r = build_dictionary[VOCAB.int2chars(int(res))]
+    bvals = [torch.tensor(b, dtype=torch.float32) for b in r["bonds-vals"]]
+    avals = [torch.tensor(a, dtype=torch.float32) for a in r["angles-vals"]]
+    tvals = [
+        torch.tensor(t, dtype=torch.float32) if t not in ["p", "i"] else t
+        for t in r["torsion-vals"]
+    ]
+    return iter(
+        zip(bvals, avals, tvals, [t.split("-") for t in r["torsion-names"]]))
+
+
+if __name__ == '__main__':
+    a = get_residue_build_iter("ALA", SC_BUILD_INFO)
+    b = get_residue_build_iter("ARG", SC_BUILD_INFO)
+    c = get_residue_build_iter("TYR", SC_BUILD_INFO)
+    for i in a:
+        print(i)
+    print("Arginine:")
+    for i in b:
+        print(f"\t{i}")
+    print("Tyrosine:")
+    for i in c:
+        print(f"\t{i}")
