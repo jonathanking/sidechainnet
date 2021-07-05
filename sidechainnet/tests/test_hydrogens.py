2bc52a18ce3f26aa0b820a24f24e34450970809e
Jonathan King <jok120@pitt.edu>
2021-07-05 19:55:06 -0400

    Update tests.

--- sidechainnet/tests/test_hydrogens.py
+++ sidechainnet/tests/test_hydrogens.py
@@ -1,11 +1,21 @@
+import torch
 import sidechainnet as scn
 
 from sidechainnet.data_handlers import SCNDataset, SCNProtein
 
-def test_add_hydrogen():
+def test_add_hydrogen_numpy():
     d = scn.load("debug", scn_dir="/home/jok120/openmm_loss/sidechainnet_data")
     d = SCNDataset(d)
     p = d['40#2BDS_1_A']  # Starts with 2 Alanines
     p.coords = p.coords[:28, :]
+    p.seq = p.seq[:2]
+    p.add_hydrogens()
+
+
+def test_add_hydrogen_torch():
+    d = scn.load("debug", scn_dir="/home/jok120/openmm_loss/sidechainnet_data")
+    d = SCNDataset(d)
+    p = d['40#2BDS_1_A']  # Starts with 2 Alanines
+    p.coords = torch.tensor(p.coords[:28, :])
     p.seq = p.seq[:2]
     p.add_hydrogens()
